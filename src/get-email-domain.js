27ca56576319dc5bed51050e1e9ab36640c6001c
Alexander Voronenko <796546@gmail.com>
2022-10-17 09:31:49 +0300

    feat: add carbon-dating solution

--- src/get-email-domain.js
+++ src/get-email-domain.js
@@ -12,12 +12,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 function getEmailDomain(/* email */) {
   throw new NotImplementedError('Not implemented');
-  //1 remove line with error and write your code here
-  //2 remove line with error and write your code here
-  //3 remove line with error and write your code here
-
-
-
+  // remove line with error and write your code here
 }
 
 module.exports = {
